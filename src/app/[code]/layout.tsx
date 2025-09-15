111587b877f5f3d39515727a2bc43f0fdb528e18
ragnsan <ragnsanfv@gmail.com>
2025-09-15 18:25:24 +0800

    Update .gitignore to include local environment files, add new environment pull scripts to package.json, and comment out static parameters in layout.tsx for future reference.

--- src/app/[code]/layout.tsx
+++ src/app/[code]/layout.tsx
@@ -1,9 +1,9 @@
 import type { ReactNode } from "react";
 import { Navbar } from "@/components/navbar";
 
-export function generateStaticParams() {
-  return [{ code: "control" }, { code: "test" }];
-}
+// export function generateStaticParams() {
+//   return [{ code: "control" }, { code: "test" }];
+// }
 
 export default async function Layout({
   children,
